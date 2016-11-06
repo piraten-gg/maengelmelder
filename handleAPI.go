8d312fc30a736b754ab75052cee85a7f69a240d7
codedust <codedust@users.noreply.github.com>
2016-11-06 21:45:05 +0100

    Use github.com/gocraft/web for request routing

--- handleAPI.go
+++ handleAPI.go
@@ -3,14 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gocraft/web"
 	"log"
 	"math/rand"
-	"net/http"
-	"strings"
 	"time"
 )
 
-var handleAPI = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func ApiPreMiddleware(w web.ResponseWriter, r *web.Request, next web.NextMiddlewareFunc) {
 	w.Header().Set("Cache-Control", "no-cache")
 
 	if storage == nil {
@@ -18,94 +17,75 @@ var handleAPI = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		rejectWithDefaultErrorJSON(w)
 		return
 	}
+	next(w, r)
+}
 
-	request := r.URL.Path[len("/api"):]
-	log.Println("[handleAPI]", request)
+func getMarkers(w web.ResponseWriter, r *web.Request) {
+	markers := storage.GetMarkers()
+	if markers == nil {
+		fmt.Println("Error fetching markers.")
+	}
+	jsonMarkers, _ := json.Marshal(markers)
+	fmt.Fprintf(w, string(jsonMarkers))
+}
 
-	switch {
-	// GET REQUESTS
-	case strings.HasPrefix(request, "/"):
-		switch request {
-		case "/markers":
-			markers := storage.GetMarkers()
-			if markers == nil {
-				fmt.Println("Error fetching markers.")
-			}
-			jsonMarkers, _ := json.Marshal(markers)
-			fmt.Fprintf(w, string(jsonMarkers))
+func getNewMarker(w web.ResponseWriter, r *web.Request) {
+	// TODO: remove this (for testing purposes only)
+	type Issue struct {
+		Lat float32 `json:"lat"`
+		Lon float32 `json:"lon"`
+	}
 
-		case "/newmarker":
-			// TODO: remove this (for testing purposes only)
-			type Issue struct {
-				Lat float32 `json:"lat"`
-				Lon float32 `json:"lon"`
-			}
+	var issue Issue
 
-			var issue Issue
+	random := rand.New(rand.NewSource(time.Now().Unix()))
 
-			r := rand.New(rand.NewSource(time.Now().Unix()))
+	issue.Lat = 49.9008 - 0.2 + random.Float32()*0.36
+	issue.Lon = 8.3500 - 0.05 + random.Float32()*0.3
 
-			issue.Lat = 49.9008 - 0.2 + r.Float32()*0.36
-			issue.Lon = 8.3500 - 0.05 + r.Float32()*0.3
+	osmmarker, err := fetchGeoInformation(issue.Lat, issue.Lon,
+		16)
 
-			osmmarker, err := fetchGeoInformation(issue.Lat, issue.Lon,
-				16)
+	if err != nil {
+		fmt.Println("Fetching geo information failed.")
+	}
 
-			if err != nil {
-				fmt.Println("Fetching geo information failed.")
-			}
-
-			if osmmarker.Address.County == CFG_OWN_COUNTY {
-				if err = storage.StoreMarker(issue.Lat, issue.Lon, osmmarker.DisplayName); err != nil {
-					fmt.Println("Error storing marker", err.Error())
-				}
-			} else {
-				rejectWithErrorJSON(w, "invalidmarker", "Marker outside of boundaries.")
-				return
-			}
-
-			a, _ := json.Marshal(issue)
-			fmt.Fprintf(w, string(a))
-
-		default:
-			// unknown GET request
-			rejectWithDefaultErrorJSON(w)
-			return
+	if osmmarker.Address.County == CFG_OWN_COUNTY {
+		if err = storage.StoreMarker(issue.Lat, issue.Lon, osmmarker.DisplayName); err != nil {
+			fmt.Println("Error storing marker", err.Error())
 		}
+	} else {
+		rejectWithErrorJSON(w, "invalidmarker", "Marker outside of boundaries.")
+		return
+	}
 
-	// POST REQUESTS
-	case strings.HasPrefix(request, "/post/"):
-		requestContent := make([]byte, r.ContentLength)
-		bytesRead, err := r.Body.Read(requestContent)
-		if err != nil && bytesRead == 0 {
-			rejectWithDefaultErrorJSON(w)
-			return
-		}
+	a, _ := json.Marshal(issue)
+	fmt.Fprintf(w, string(a))
+}
 
-		switch request {
-		case "/post/marker":
-			type marker struct {
-				Lat float32 `json:"lat"`
-				Lon float32 `json:"lon"`
-			}
-
-			var data marker
-			if err = json.Unmarshal(requestContent, &data); err != nil {
-				rejectWithDefaultErrorJSON(w)
-				return
-			}
-
-			fmt.Println("New marker: ", data.Lat, data.Lon)
-
-		default:
-			// unknown POST request
-			rejectWithDefaultErrorJSON(w)
-			return
-		}
-
-	default:
-		// unknown API request
+func newMarker(w web.ResponseWriter, r *web.Request) {
+	requestContent := make([]byte, r.ContentLength)
+	bytesRead, err := r.Body.Read(requestContent)
+	if err != nil && bytesRead == 0 {
 		rejectWithDefaultErrorJSON(w)
 		return
 	}
-})
+
+	type marker struct {
+		Lat          float32 `json:"lat"`
+		Lon          float32 `json:"lon"`
+		Category     int     `json:"category"`
+		Desc         string  `json:"desc"`
+		Confidential bool    `json:"confidential"`
+		UserName     string  `json:"user_name"`
+		UserMail     string  `json:"user_mail"`
+	}
+
+	var data marker
+	if err = json.Unmarshal(requestContent, &data); err != nil {
+		rejectWithErrorJSON(w, "couldNotParse", "Could not parse data.")
+		return
+	}
+
+	fmt.Println("New marker: ", data.Lat, data.Lon)
+}
--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/codedust/go-httpserve"
+	"github.com/gocraft/web"
 	"github.com/piraten-gg/maengelmelder/persistence"
 	"log"
-	"net/http"
 )
 
 // the global connection to the database
 var storage *persistence.StorageConn
+
+type Context struct {}
 
 func main() {
 	var err error
@@ -20,10 +22,19 @@ func main() {
 	}
 
 	// setup web server
-	mux := http.NewServeMux()
-	mux.Handle("/events", http.Handler(handleWS))
-	mux.Handle("/api/", http.Handler(handleAPI))
-	mux.Handle("/", http.FileServer(http.Dir(CFG_HTML_DIR)))
+	router := web.New(Context{})
+	router.Middleware(web.LoggerMiddleware)
+	router.Middleware(web.ShowErrorsMiddleware) // TODO: only in dev environment
+	router.Middleware(ApiPreMiddleware)
+	router.Middleware(web.StaticMiddleware(
+		CFG_HTML_DIR, web.StaticOption{IndexFile: "index.html"}))
+	router.Get("/ws", func(rw web.ResponseWriter, req *web.Request) {
+		handleWS.ServeHTTP(rw, req.Request)
+	})
+
+	router.Get("/api/v1/markers/show", getMarkers)
+	router.Get("/api/v1/markers/generate", getNewMarker)
+	router.Post("/api/v1/markers/create", getNewMarker)
 
 	// create or load certificate
 	CreateCertificateIfNotExist(
@@ -39,7 +50,7 @@ func main() {
 		"0.0.0.0:"+CFG_SERVER_PORT,
 		CFG_DATA_DIR+CFG_CERT_PREFIX+"cert.pem",
 		CFG_DATA_DIR+CFG_CERT_PREFIX+"key.pem",
-		mux)
+		router)
 
 	log.Fatal(err)
 }
